aafcba11992f59ebe8306f88af73db9be67df445
Kuroneko188 <2011078069@qq.com>
2024-07-13 16:54:52 +0800

    Initial Commit

---
+++ GithubTest1/GithubTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTest1App.swift
+//  GithubTest1
+//
+//  Created by Li Xianlin on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
